0ba3d68ef25e5531ce0e9f2d72a955cd77c86e06
electroo0 <90555965+amitjimiwal@users.noreply.github.com>
2024-06-13 17:44:01 +0530

    updates

--- backend/src/db/feedbackdb.ts
+++ backend/src/db/feedbackdb.ts
@@ -2,12 +2,19 @@ import Feedback from "../lib/types/feedback.js";
 
 const feedbacks: Feedback[] = [{
      name: "John Doe",
-     feedback: "Great work",
+     feedback: "Great feedback Management System",
      time: new Date(),
-},{
-     name: "Jane Doe",
-     feedback: "Nice work",
+}, {
+     name: "Amit",
+     feedback: "I love this website. I have used many feedback websites but this is the best one. I will recommend this to my friends",
      time: new Date(),
 }];
-
+//generate feedbacks
+for (let i = 0; i < 100; i++) {
+     feedbacks.push({
+          name: `User ${i}`,
+          feedback: `This is the feedback from user ${i}`,
+          time: new Date(),
+     });
+}
 export default feedbacks;
